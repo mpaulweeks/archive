4c7c5af4249d82267893345490894ec05b5ea28e
mpaulweeks <mpaulweeks@gmail.com>
2018-11-08 10:11:38 -0500

    try lower qualities if 1080 not found

--- py/archive/youtube.py
+++ py/archive/youtube.py
@@ -1,15 +1,28 @@
 
 import youtube_dl
 
+# https://www.genyoutube.net/formats-resolution-youtube-videos.html
+format_prefs = [
+    '137', # mp4 1080p
+    '136', # mp4 720p
+    '135', # mp4 480p
+]
+
 def extract_youtube(vid, filename):
     url = 'https://youtube.com/watch?v=%s' % vid
     ydl_opts = {
         'outtmpl': 'temp/%s' % filename,
-        'format': '137', # mp4 1080p
     }
-    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-        ydl.download([url])
-        return filename
+    for format in format_prefs:
+        ydl_opts['format'] = format
+        try:
+            with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+                ydl.download([url])
+                return filename
+        except Exception as e:
+            print(e)
+            continue
+    return False
 
 import sys
 if __name__ == '__main__':
